4db7e522173615a6746aaa4aeedcf2230c35b2f6
Fred Morcos <fm@fredmorcos.com>
2022-03-16 21:19:55 +0100

    Squashed commit of the following:
    commit f93635b58904a4b591466e5d604a8fcc4cac3838
    Author: Fred Morcos <fm@fredmorcos.com>
    Date:   Wed Mar 16 21:19:10 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 79714236df08406934cc628cb7e41db45e440440
    Author: Koushik Meenakshisundaram <koushik.ms@gmail.com>
    Date:   Wed Mar 16 20:03:36 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 04cc5317a15e8f0aa46c94c40a7d183d3ff66a76
    Author: dpolivaev <dpolivaev@gmx.de>
    Date:   Wed Mar 16 19:58:40 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit beb8c25e8ad59da1f05992e345939ce81371f43f
    Author: Henning Holm <git@henningholm.de>
    Date:   Wed Mar 16 20:55:06 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 1ee8cb60f1bf852ff44c41b8b7a23578d0fa2123
    Author: Dhanoa <VAISHALI.DHANOA@gmail.com>
    Date:   Wed Mar 16 19:50:04 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 9e632b876dfb35b5482fa3812fc2abe0d6618706
    Author: Fred Morcos <fm@fredmorcos.com>
    Date:   Wed Mar 16 20:46:16 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 663affa2dbba4e9adbe63fd23f42b2518a6e0a23
    Author: mob-programming-meetup <dawid.szewczyk@outlook.com>
    Date:   Wed Mar 16 19:42:49 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 8fd32484304ed9099f6b13cf48f077d57aa7ab3a
    Author: Koushik Meenakshisundaram <koushik.ms@gmail.com>
    Date:   Wed Mar 16 19:38:24 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 748289e793a9e1f607b34a17890ca68f5d1782d1
    Author: dpolivaev <dpolivaev@gmx.de>
    Date:   Wed Mar 16 19:31:25 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit ebba2dc9658396cdfa3b7f6f91f29feb884698f9
    Author: Henning Holm <git@henningholm.de>
    Date:   Wed Mar 16 20:27:23 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 8c47279ea85eacae9c7a2f5f38032f5f51dbd1c5
    Author: Fred Morcos <fm@fredmorcos.com>
    Date:   Wed Mar 16 20:18:27 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit b24781a290f8ac793223c52a3c915fec58d40be6
    Author: Alberto Rodríguez García <alberto.rodriguezgarcia@nsl.nl>
    Date:   Wed Mar 16 19:50:41 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit e0b90fe5979702e9831d8cc4678f9f9f5b9e9004
    Author: Koushik Meenakshisundaram <koushik.ms@gmail.com>
    Date:   Wed Mar 16 18:46:02 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 9dcf1ac1bf03771b6d91fc07d66983d6198e9d32
    Author: dpolivaev <dpolivaev@gmx.de>
    Date:   Wed Mar 16 18:42:04 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit ad891b5262315eae066bcef3a89550c1435ad34e
    Author: Henning Holm <git@henningholm.de>
    Date:   Wed Mar 16 19:37:52 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit c9e8b617d1407f5145df7e5950d7cdb7f7f67625
    Author: Fred Morcos <fm@fredmorcos.com>
    Date:   Wed Mar 16 19:30:27 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 8ed742d1da3ec4c9628b0f77df35380e7a685d2f
    Author: Alberto Rodríguez García <alberto.rodriguezgarcia@nsl.nl>
    Date:   Wed Mar 16 19:26:44 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 36b307e333d8465abd1592eab2c151eaf6c7ca54
    Author: Koushik Meenakshisundaram <koushik.ms@gmail.com>
    Date:   Wed Mar 16 18:22:05 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 25319a14091854351d5ed52f4f585a7b2a8cf3bc
    Author: dpolivaev <dpolivaev@gmx.de>
    Date:   Wed Mar 16 18:13:29 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 82c0535ec24055ea6a53b1d5226318b7ba8d2166
    Author: Henning Holm <git@henningholm.de>
    Date:   Wed Mar 16 19:09:11 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    Co-authored-by: dpolivaev <dpolivaev@gmx.de>
    Co-authored-by: Henning Holm <git@henningholm.de>
    Co-authored-by: Dhanoa <VAISHALI.DHANOA@gmail.com>
    Co-authored-by: Koushik Meenakshisundaram <koushik.ms@gmail.com>
    Co-authored-by: mob-programming-meetup <dawid.szewczyk@outlook.com>
    Co-authored-by: Alberto Rodríguez García <alberto.rodriguezgarcia@nsl.nl>

--- src/day04.rs
+++ src/day04.rs
@@ -1,22 +1,23 @@
 use std::str::FromStr;
 
-/*
-
-Ideas:
-1.bingo struct with from vec[string] - including board and sequences
-2. parsing the string to create the matrix
-3. parse the input list of numbers (drawn)
-4. implement - mark the number on the board
-5. check whether a specific row or column is fully marked
-6. add a function that determines if a board is winning or not
-7. implement scoring function
-8. split input into draws and board definitions,
-9. split board definition into a vector of boards
-
- */
+// Ideas:
+// 01. bingo struct with from vec[string] - including board and sequences
+// 02. parsing the string to create the matrix
+// 03. parse the input list of numbers (drawn)
+// 04. implement - mark the number on the board
+// 05. check whether a specific row or column is fully marked
+// 06. add a function that determines if a board is winning or not
+//  a. possible impl could check each row & col for a win
+//  b. could be made more efficient
+// 07. implement scoring function
+// 08. split input into draws and board definitions,
+// 09. split board definition into a vector of boards
+// 10. keep track of marked cells in a matrix of bool
+// 11. to represent numbers with the times-table
 
 struct Board {
   cells: Vec<Vec<u8>>,
+  marked: Vec<Vec<bool>>,
 }
 
 impl Board {
@@ -29,18 +30,33 @@ impl Board {
     false
   }
 
-  fn locate(&self, n: u8) -> (u8, u8) {
-    for row in &self.cells {
-      if row.contains(&n) {
-        return (2, 4);
+  fn locate(&self, n: u8) -> Option<(usize, usize)> {
+    for (row_id, row) in self.cells.iter().enumerate() {
+      for (col_id, &cell) in row.iter().enumerate() {
+        if cell == n {
+          return Some((row_id, col_id));
+        }
       }
     }
-    todo!()
+    None
   }
 
-  fn mark(&self, _: u8) {}
-  fn is_marked(&self, row: u8, col: u8) -> bool {
-    true
+  fn mark(&mut self, n: u8) {
+    if let Some((row, col)) = self.locate(n) {
+      self.marked[row][col] = true;
+    }
+  }
+
+  fn is_marked(&self, row: usize, col: usize) -> bool {
+    self.marked[row][col]
+  }
+
+  fn row_is_marked(&self, row: usize) -> bool {
+    self.marked[row].iter().all(|&cell| cell)
+  }
+
+  fn col_is_marked(&self, col: usize) -> bool {
+    self.marked.iter().all(|row| row[col])
   }
 }
 
@@ -48,7 +64,7 @@ impl FromStr for Board {
   type Err = ();
 
   fn from_str(s: &str) -> Result<Self, Self::Err> {
-    let cells = s
+    let cells: Vec<Vec<u8>> = s
       .lines()
       .map(|line| {
         line
@@ -57,7 +73,8 @@ impl FromStr for Board {
           .collect()
       })
       .collect();
-    Ok(Self { cells })
+    let marked = vec![vec![false; cells[0].len()]; cells.len()];
+    Ok(Self { cells, marked })
   }
 }
 
@@ -75,18 +92,44 @@ mod tests {
   }
 
   #[test]
-  fn locates_nuber_on_board() {
+  fn locates_number_on_board() {
     let board = Board::from_str(&create_board_data()).unwrap();
-    assert_eq!(board.locate(7), (2, 4));
-    assert_eq!(board.locate(0), (0, 4))
+    assert_eq!(board.locate(7), Some((2, 4)));
+    assert_eq!(board.locate(0), Some((0, 4)));
+    assert_eq!(board.locate(8), Some((1, 0)));
+    assert_eq!(board.locate(99), None);
   }
 
   #[test]
   fn board_marks_positions() {
-    let board = Board::from_str(&create_board_data()).unwrap();
+    let mut board = Board::from_str(&create_board_data()).unwrap();
     board.mark(7);
     assert!(board.is_marked(2, 4));
-    // assert!(!board.is_marked(1, 4));
+    assert!(!board.is_marked(1, 4));
+  }
+
+  #[test]
+  fn board_row_is_marked() {
+    let mut board = Board::from_str(&create_board_data()).unwrap();
+    board.mark(8);
+    board.mark(2);
+    board.mark(23);
+    board.mark(4);
+    board.mark(24);
+    assert!(board.row_is_marked(1));
+    assert!(!board.row_is_marked(0));
+  }
+
+  #[test]
+  fn board_column_is_marked() {
+    let mut board = Board::from_str(&create_board_data()).unwrap();
+    board.mark(13);
+    board.mark(2);
+    board.mark(9);
+    board.mark(10);
+    board.mark(12);
+    assert!(board.col_is_marked(1));
+    assert!(!board.col_is_marked(0));
   }
 
   //TODO: Change this to a static string
